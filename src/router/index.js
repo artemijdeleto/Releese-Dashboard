af36ac5dbe468a872c0908cac43379726b2e6579
Artemij <artemijdeleto@gmail.com>
2020-07-13 19:24:09 +0400

    Add login view

--- src/router/index.js
+++ src/router/index.js
@@ -2,9 +2,15 @@ import Vue from 'vue'
 import Router from 'vue-router'
 import store from '@/store'
 
+import Login from '@/views/Login.vue'
+
 Vue.use(Router);
 
 const routes = [
+	{
+		path: '/login',
+		component: Login
+	}
 ];
 
 const router = new Router({
---
+++ src/views/Login.vue
@@ -0,0 +1,10 @@
+<template>
+	<div class="container text-center">
+		<h1 class="heading">Releese</h1>
+		<a v-once
+		   :href="$store.state.ACCESS_ROOT + '/#/grant/releese'"
+		>
+			Proceed to Desire Access
+		</a>
+	</div>
+</template>
